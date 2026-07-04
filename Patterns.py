f29fe67d7553108ffe36027365999105e5c4f5d4
parameshyadav072-afk <parameshyadav072@gmail.com>
2026-07-04 12:38:10 +0530

    Add files via upload

---
+++ Patterns.py
@@ -0,0 +1,84 @@
+# #  Square Pattern 
+# r=4
+# for i in range(r):
+#     for j in range(r):
+#         print("*",end=" ")
+#     print()
+
+
+# #  Right Triangle
+# r=5
+# for i in range(r):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# #  Number Triangle 
+# r=6
+# for i in range(r):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# # 4. Repeated Number Triangle 
+# r=6
+# for i in range(r):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+
+# #  AlphabetTriangle 
+# r=5
+# for i in range(r):
+#     for j in range(0,i+1):
+#         print(chr(65+j),end=" ")
+#     print()
+
+
+# #Inverted Star Triangle
+# r=5
+# for i in range(r-1,-1,-1):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# #Inverted Number Triangle
+# r=6
+# for i in range(r-1,-1,-1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+
+# #Continuous Number Pattern
+# r=6
+# count=1
+# for i in range(r):
+#     for j in range(1,i+1):
+#         print(count,end=" ")
+#         count+=1
+#     print()
+
+
+# # Right-Aligned Star Triangle 
+# n=5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+
+
+# #  Pyramid Pattern    
+# n=5
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
