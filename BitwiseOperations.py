5baff9e8f1328c08f880105fdfbbff2188e9a48e
parameshyadav072-afk <parameshyadav072@gmail.com>
2026-07-04 12:26:49 +0530

    Add files via upload

---
+++ BitwiseOperations.py
@@ -0,0 +1,28 @@
+#If two values are same values perform “XOR” operation 
+# # Perform “XOR” be if two values are same what will be the output.   
+a=int(input("Enter"))
+b=int(input("Enter"))
+xor=a ^ b
+print(xor)
+
+
+# Perform and operation 1 is even? Or odd? What will the output?  
+a=int(input("Enter"))
+b=1
+and_op=a &  b
+print(and_op)
+
+a=int(input("Enter"))
+b=1
+and_op=a &  b
+print(and_op)
+
+# Practice all bitwise operator. 
+a=int(input("Enter"))
+b=int(input("Enter"))
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
+print(a>>2)
+print(b<<2)
