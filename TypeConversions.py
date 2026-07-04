6a0fcc1b24fbf49272ab9875ace23ce879e1066d
parameshyadav072-afk <parameshyadav072@gmail.com>
2026-07-04 12:28:39 +0530

    Add files via upload

---
+++ TypeConversions.py
@@ -0,0 +1,95 @@
+# 1. int -> float
+a = 10
+print(float(a))          
+
+# 2. int -> str
+a = 10
+print(str(a))           
+
+# 3. int -> complex
+a = 10
+print(complex(a))        
+
+# 4. float -> int
+a = 10.5
+print(int(a))            
+
+# 5. float -> str
+a = 10.5
+print(str(a))           
+
+# 6. float -> complex
+a = 10.5
+print(complex(a))       
+
+# 7. str -> int
+a = "100"
+print(int(a))          
+
+# 8. str -> float
+a = "100.5"
+print(float(a))          
+
+# 9. str -> complex
+a = "10+5j"
+print(complex(a))        
+
+# 10. str -> list
+a = "python"
+print(list(a))           
+
+# 11. str -> tuple
+a = "python"
+print(tuple(a))         
+
+# 12. str -> set
+a = "python"
+print(set(a))            
+
+# 13. list -> tuple
+a = [1,2,3]
+print(tuple(a))          
+
+# 14. list -> set
+a = [1,2,2,3]
+print(set(a))            
+
+# 15. list -> str
+a = ['p','y','t','h','o','n']
+print("".join(a))        
+
+# 16. tuple -> list
+a = (1,2,3)
+print(list(a))           
+
+# 17. tuple -> set
+a = (1,2,2,3)
+print(set(a))            
+
+# 18. tuple -> str
+a = ('p','y','t','h','o','n')
+print("".join(a))        
+
+# 19. set -> list
+a = {1,2,3}
+print(list(a))           
+
+# 20. set -> tuple
+a = {1,2,3}
+print(tuple(a))          
+
+# 21. set -> str
+a = {'p','y','t','h','o','n'}
+print("".join(a))        
+
+# 22. dict -> list
+a = {'a':1,'b':2}
+print(list(a))           
+
+# 23. dict -> tuple
+a = {'a':1,'b':2}
+print(tuple(a))          
+
+# 24. dict -> set
+a = {'a':1,'b':2}
+print(set(a))            
